1cde2237bd79211f91201326d3c6fa78c2ceb23e
miguelmartinez <migueemartineez24@uma.es>
2026-05-10 19:08:50 +0200

    Actualizamos los campos necesarios para Animal.js

--- Backend-zoo-app/models/Animal.js
+++ Backend-zoo-app/models/Animal.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 
 const animalSchema = new mongoose.Schema({
     nombre: { type: String, required: true },
-    descripcion: { type: String, required: true },
-    ecosistema: { type: String, required: true }, // ej: 'África subsahariana'
-    tipoAnimal: { type: String, required: true },  // ej: 'mamífero'
-    interesTematico: [String], // ej: ['conservación', 'comportamiento']
-    imagen: { type: String },  // URL de ImageKit
+    descripcion: { type: String }, // ej: Biología y Comportamiento
+    gradoAmenaza: { type: String },
+    dieta: { type: String },
+    curiosidades: { type: String },
+    vida: { type: String },
+    ecosistema: { type: String }, // ej: 'África ecuatorial'
+    tipoAnimal: { type: String }, // ej: mamífero
+    imagen: { type: String }, //URL de ImageKit
     zonaId: { type: mongoose.Schema.Types.ObjectId, ref: 'Zona', required: true }
 });
 
